4ad7f4c83e48a4e3849214a73c444b9e084d33f4
Vladyslav Zaporozhskyi <zapovlad@gmail.com>
2023-08-27 11:36:12 -0400

    Pass the text from the input to the Figma file

--- code.ts
+++ code.ts
@@ -1,11 +1,16 @@
 // Show the plugin UI
 figma.showUI(__html__, {width: 400, height:400});
 
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = async msg => {
+
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+
   if (msg.type === 'actionGenerate') {
 
     // Desctructure the form data object
     const {invoiceTitle, invoiceNumber, invoiceDate, frameDirection} = msg.formDataObj
+
+    const title = invoiceTitle
 
     // Create the frame and name it
     const parentFrame = figma.createFrame()
@@ -13,8 +18,22 @@ figma.ui.onmessage = msg => {
     parentFrame.name = invoiceTitle + " " + invoiceNumber
 
     parentFrame.layoutMode = frameDirection.toUpperCase() 
-    parentFrame.horizontalPadding = 32
     parentFrame.itemSpacing = 8
+
+    parentFrame.primaryAxisSizingMode = 'FIXED'
+    parentFrame.counterAxisSizingMode = 'FIXED'
+
+    // Create the text property in Figma
+    const titleNode = figma.createText()
+
+    // Name the layer
+    titleNode.name = invoiceTitle
+
+    // Generate the input text into the text property in Figma
+    titleNode.characters = title.toString()
+
+    // Size the layer
+    titleNode.resize(346, 36)
 
     // Close the plugin 
     figma.closePlugin('Your invoice is generated. Get that 💰')
