dce29c3df8b1ee7798b15e818a52fffd239de046
Vladyslav Zaporozhskyi <zapovlad@gmail.com>
2023-10-24 20:29:46 -0400

    Refactor naming and code spacing

--- code.ts
+++ code.ts
@@ -15,7 +15,6 @@ figma.ui.onmessage = async msg => {
     }
 
     // Desctructure the form data object
-
     const {
       invoiceTitle,
       invoiceNumber,
@@ -39,10 +38,6 @@ figma.ui.onmessage = async msg => {
 
     const title = invoiceTitle
     const number = invoiceNumber
-    const from = legendSenderLabel
-    const sender = senderName
-    const to = legendReceiverLabel
-    const receiver = receiverName
 
     // Create the parent frame and name it
     const parentFrame = figma.createFrame()
@@ -86,32 +81,32 @@ figma.ui.onmessage = async msg => {
     titleFrame.counterAxisSizingMode = 'AUTO'
     titleFrame.counterAxisAlignItems = 'CENTER'
 
-    const contactsFrame = figma.createFrame()
-    contactsFrame.name = 'Contacts'
-    contactsFrame.layoutMode = 'HORIZONTAL'
-    contactsFrame.itemSpacing = 24
-    contactsFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
-    contactsFrame.resize(528, 112)
-    contactsFrame.primaryAxisSizingMode = 'FIXED'
-    contactsFrame.counterAxisSizingMode = 'AUTO'
+    const contactInfoFrame = figma.createFrame()
+    contactInfoFrame.name = 'Contacts'
+    contactInfoFrame.layoutMode = 'HORIZONTAL'
+    contactInfoFrame.itemSpacing = 24
+    contactInfoFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
+    contactInfoFrame.resize(528, 112)
+    contactInfoFrame.primaryAxisSizingMode = 'FIXED'
+    contactInfoFrame.counterAxisSizingMode = 'AUTO'
 
-    const senderNameFrame = figma.createFrame()
-    senderNameFrame.name = 'Sender'
-    senderNameFrame.layoutMode = 'VERTICAL'
-    senderNameFrame.itemSpacing = 8
-    senderNameFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
-    senderNameFrame.resize(252, 44)
-    senderNameFrame.primaryAxisSizingMode = 'AUTO'
-    senderNameFrame.counterAxisSizingMode = 'FIXED'
+    const senderFrame = figma.createFrame()
+    senderFrame.name = 'Sender'
+    senderFrame.layoutMode = 'VERTICAL'
+    senderFrame.itemSpacing = 8
+    senderFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
+    senderFrame.resize(252, 44)
+    senderFrame.primaryAxisSizingMode = 'AUTO'
+    senderFrame.counterAxisSizingMode = 'FIXED'
 
-    const receiverNameFrame = figma.createFrame()
-    receiverNameFrame.name = 'Receiver'
-    receiverNameFrame.layoutMode = 'VERTICAL'
-    receiverNameFrame.itemSpacing = 8
-    receiverNameFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
-    receiverNameFrame.resize(252, 44)
-    receiverNameFrame.primaryAxisSizingMode = 'AUTO'
-    receiverNameFrame.counterAxisSizingMode = 'FIXED'
+    const receiverFrame = figma.createFrame()
+    receiverFrame.name = 'Receiver'
+    receiverFrame.layoutMode = 'VERTICAL'
+    receiverFrame.itemSpacing = 8
+    receiverFrame.fills = [{type : 'SOLID', color: {r: 0.973, g: 0.965, b: 0.945}}]
+    receiverFrame.resize(252, 44)
+    receiverFrame.primaryAxisSizingMode = 'AUTO'
+    receiverFrame.counterAxisSizingMode = 'FIXED'
 
     // Create the title text property in Figma
     const titleNode = figma.createText()
@@ -135,73 +130,76 @@ figma.ui.onmessage = async msg => {
     dateNode.letterSpacing = {unit: 'PERCENT', value: -1}
     dateNode.fills = [{type: 'SOLID', color: {r: 0.31, g: 0.302, b: 0.29}}]
 
-    const fromNode = figma.createText()
-    fromNode.fontName = { family: 'Inter', style: 'Regular' }
-    fromNode.fontSize = 12
-    fromNode.lineHeight = {unit: 'PIXELS', value: 16}
-    fromNode.letterSpacing = {unit: 'PERCENT', value: -1}
-    fromNode.fills = [{type: 'SOLID', color: {r: 0.576, g: 0.561, b: 0.545}}]
+    const senderLabelNode = figma.createText()
+    senderLabelNode.fontName = { family: 'Inter', style: 'Regular' }
+    senderLabelNode.fontSize = 12
+    senderLabelNode.lineHeight = {unit: 'PIXELS', value: 16}
+    senderLabelNode.letterSpacing = {unit: 'PERCENT', value: -1}
+    senderLabelNode.fills = [{type: 'SOLID', color: {r: 0.576, g: 0.561, b: 0.545}}]
 
-    const senderNode = figma.createText()
-    senderNode.fontName = {family: 'Unbounded', style: 'Regular'}
-    senderNode.fontSize = 16
-    senderNode.lineHeight = {unit: 'PIXELS', value: 20}
-    senderNode.letterSpacing = {unit: 'PERCENT', value: -2}
-    senderNode.fills = [{type: 'SOLID', color: {r: 0.145, g: 0.133, b: 0.125}}]
+    const senderNameNode = figma.createText()
+    senderNameNode.fontName = {family: 'Unbounded', style: 'Regular'}
+    senderNameNode.fontSize = 16
+    senderNameNode.lineHeight = {unit: 'PIXELS', value: 20}
+    senderNameNode.letterSpacing = {unit: 'PERCENT', value: -2}
+    senderNameNode.fills = [{type: 'SOLID', color: {r: 0.145, g: 0.133, b: 0.125}}]
 
-    const toNode = figma.createText()
-    toNode.fontName = { family: 'Inter', style: 'Regular' }
-    toNode.fontSize = 12
-    toNode.lineHeight = {unit: 'PIXELS', value: 16}
-    toNode.letterSpacing = {unit: 'PERCENT', value: -1}
-    toNode.fills = [{type: 'SOLID', color: {r: 0.576, g: 0.561, b: 0.545}}]
+    const receiverLabelNode = figma.createText()
+    receiverLabelNode.fontName = { family: 'Inter', style: 'Regular' }
+    receiverLabelNode.fontSize = 12
+    receiverLabelNode.lineHeight = {unit: 'PIXELS', value: 16}
+    receiverLabelNode.letterSpacing = {unit: 'PERCENT', value: -1}
+    receiverLabelNode.fills = [{type: 'SOLID', color: {r: 0.576, g: 0.561, b: 0.545}}]
 
-    const receiverNode = figma.createText()
-    receiverNode.fontName = {family: 'Unbounded', style: 'Regular'}
-    receiverNode.fontSize = 16
-    receiverNode.lineHeight = {unit: 'PIXELS', value: 20}
-    receiverNode.letterSpacing = {unit: 'PERCENT', value: -2}
-    receiverNode.fills = [{type: 'SOLID', color: {r: 0.145, g: 0.133, b: 0.125}}]
+    const receiverNameNode = figma.createText()
+    receiverNameNode.fontName = {family: 'Unbounded', style: 'Regular'}
+    receiverNameNode.fontSize = 16
+    receiverNameNode.lineHeight = {unit: 'PIXELS', value: 20}
+    receiverNameNode.letterSpacing = {unit: 'PERCENT', value: -2}
+    receiverNameNode.fills = [{type: 'SOLID', color: {r: 0.145, g: 0.133, b: 0.125}}]
 
     // Name the layer
     titleNode.name = title
     numberNode.name = number
     dateNode.name = formattedDate
-    fromNode.name = legendSenderLabel
-    toNode.name = legendReceiverLabel
-    senderNode.name = sender
-    receiverNode.name = receiver
+
+    senderLabelNode.name = legendSenderLabel
+    senderNameNode.name = senderName
+
+    receiverLabelNode.name = legendReceiverLabel
+    receiverNameNode.name = receiverName
 
     // Generate the input text into the text property in Figma
     titleNode.characters = title.toString()
     numberNode.characters = number.toString()
     dateNode.characters = formattedDate.toString()
-    fromNode.characters = from.toString()
-    toNode.characters = to.toString()
-    senderNode.characters = sender.toString()
-    receiverNode.characters = receiver.toString()
 
-    // Add the title and number to the title frame
+    senderLabelNode.characters = legendSenderLabel.toString()
+    senderNameNode.characters = senderName.toString()
+
+    receiverLabelNode.characters = legendReceiverLabel.toString()
+    receiverNameNode.characters = receiverName.toString()
+
+    // Display the content in Figma
     titleFrame.appendChild(titleNode)
     titleFrame.appendChild(numberNode)
 
-    // Add the title frame to the details frame
     detailsFrame.appendChild(titleFrame)
-
-    // Add the date to the details frame
     detailsFrame.appendChild(dateNode)
-    parentFrame.appendChild(infoFrame)
 
-    senderNameFrame.appendChild(fromNode)
-    senderNameFrame.appendChild(senderNode)
-    receiverNameFrame.appendChild(toNode)
-    receiverNameFrame.appendChild(receiverNode)
+    senderFrame.appendChild(senderLabelNode)
+    senderFrame.appendChild(senderNameNode)
 
-    contactsFrame.appendChild(senderNameFrame)
-    contactsFrame.appendChild(receiverNameFrame)
+    receiverFrame.appendChild(receiverLabelNode)
+    receiverFrame.appendChild(receiverNameNode)
+
+    contactInfoFrame.appendChild(senderFrame)
+    contactInfoFrame.appendChild(receiverFrame)
 
     infoFrame.appendChild(detailsFrame)
-    infoFrame.appendChild(contactsFrame)
+    infoFrame.appendChild(contactInfoFrame)
+
+    parentFrame.appendChild(infoFrame)
 
     // Close the plugin 
     figma.closePlugin('Your invoice is generated. Get that 💰')
